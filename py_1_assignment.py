aa38c7594ca973a4c26f9c942c3e72f79d04bef9
Based-Shubham <shubhamali.pachora@gmail.com>
2026-06-03 23:53:17 +0530

    Add py_1_assignment

---
+++ py_1_assignment.py
@@ -0,0 +1,150 @@
+# task 1
+print("Hello World")
+
+# task 2
+print("Name: Shubham Mali\nClass: Third Year\nCollege : Matoshri Asarabai Polytechnic")
+
+# task 3
+print(input("Fav Colour:"))
+print(input("Fav Food :"))
+print(input("Fav Sport:"))
+
+# task 4
+Name = ("Shubham Mali")
+print(Name)
+
+# task 5
+age = 18
+height=5.6
+city="Mumbai"
+print(age)
+print(height)
+print(city)
+
+# task 6
+a = 18
+b=5.6
+c="Mumbai"
+print(type(a))
+print(type(b))
+print(type(c))
+
+# task 7
+a = 10
+print(a)
+a = 20
+print(a)
+
+# task 8
+Name = input("Enter your name:")
+print(Name)
+
+# task 9
+city = input("Enter your city")
+print(city)
+
+# task 10
+fav_sub = input("Enter your favorite subject:")
+print(fav_sub)
+
+# task 11
+age = input("Enter your age:")
+print(age)
+
+# task 12 
+no1 = input("Enter first number:")
+print(no1)
+no2 = input("Enter second number:")
+print(no2)
+
+# task 13
+a = 3
+print (float(a))
+
+# task 14
+a = 5.6
+print(int(a))
+
+# task 15
+a = 5
+print()
+# incomplete
+
+# task 16
+a = input("enter a number:")
+print(int(a))
+#check
+
+# task 17
+a = input("enter a number:")
+b = input("enter another number:")
+print(float(c = a+b))
+#check
+
+# task 18
+Name = input("Enter your Name:")
+Age = input("Enter your Age:")
+City = input("Enter your City Name:")
+print (Name)
+print(Age)
+print(City)
+#check
+
+# task 19
+a = input("Enter A number:")
+b = input("Enter another number;")
+add = a+b
+sub = a-b
+mul = a*b
+print (add)
+print(sub)
+print(mul)
+#check
+
+# task 20
+Name = input("Enter your name:")
+Age = input("Enter your age:")
+Branch = input("Enter your branch name:")
+Semister = input("Enter your semister:")
+print("Name of the Student:",Name, "\nAge of the Student:",Age,"\nBranch of the Student:",Branch, "\nCurrent Semister of the Student:",Semister)
+#check
+
+# Viva Questions
+
+""" 
+
+1. what is python?
+--> Python is a high level programming language used for Web development, artificial intelligence,etc.
+it also has the easiest syntax among all other programming languages.
+
+2. what is a variable?
+--> A variable is a container that stores a value or data and assigning it a address in the memory, making it easy for the programmer to not remember the memory address of the value. 
+
+3. What is a data type?
+--> A data type is a category that tells a computer or compiler what kind of value a piece of information is and how to use it.
+
+4. Name any four data types in Python 
+--> 1] int
+    2] float
+    3] char
+    4] str
+
+5. What is the use of input() function?
+-->input() function is used to take input values or data from the user via terminal.
+
+6. what is type casting
+--> type casting is teh process of changing a declared variables data type into another.
+
+7. how do you convert string to integer?
+--> to convert a string into integer you have to put the string variable into int() function.
+
+8. how do you check the data type of a variable?
+--> to check the data type of a variable you have to use use type() function.
+
+9. Difference between int() and float()?
+--> int() is used to declare or type cast a variable into integer format like whole numbers.
+    float() is used to declare or type cast a variable into floating point format like decimal points.
+
+10. What is the output of print(type(10))?
+--> <class 'int'>
+"""
